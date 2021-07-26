b7df4826c1f3a7fd537357972775b07d8bf09e07
German Distel <germandistel@Germans-MacBook-Air.local>
2021-07-26 18:37:30 -0300

    Fixed routes according to challenge instructions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'items',
     loadChildren: () => import('./items-list/items-list.module').then(m => m.ItemsListModule)
   },
 ];
